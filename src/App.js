9c4d258cdd719e5cc115d5a20ba7d952e621566c
Chris <christian.altenhofer@gmx.de>
2023-08-02 09:09:31 +0200

    gogo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// Importiere notwendige Funktionen von React
+import React, { useState } from 'react';
 
-function App() {
+// Definiere die Aufgaben-Komponente
+function Aufgaben() {
+  
+  // useState wird für die Verwaltung des Zustands in funktionalen Komponenten verwendet.
+  // Hier wird der Zustand für die Aufgabenliste und die neue Aufgabe definiert.
+  const [aufgaben, setAufgaben] = useState([
+    { text: 'Hausaufgaben machen', done: false },
+    { text: 'Einkaufen gehen', done: false },
+    { text: 'Bericht schreiben', done: false },
+    { text: 'Freunde treffen', done: false },
+    { text: 'Ein Buch lesen', done: false },
+  ]);
+
+  const [neueAufgabe, setNeueAufgabe] = useState('');
+
+  // Funktion zum Hinzufügen einer neuen Aufgabe
+  const addAufgabe = () => {
+    setAufgaben([...aufgaben, { text: neueAufgabe, done: false }]);
+    setNeueAufgabe('');  // Setze das Eingabefeld zurück
+  };
+
+  // Funktion zum Wechseln des "erledigt"-Status einer Aufgabe
+  const toggleDone = (index) => {
+    const updatedAufgaben = aufgaben.map((aufgabe, aufgabeIndex) =>
+      aufgabeIndex === index ? { ...aufgabe, done: !aufgabe.done } : aufgabe
+    );
+    setAufgaben(updatedAufgaben);
+  };
+
+  // Rendern der Komponente
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Aufgabenliste</h1>
+      {/* Iteriere über die Aufgabenliste und erstelle für jede Aufgabe ein checkbox-input und ein span-Element */}
+      {aufgaben.map((aufgabe, index) => (
+        <div key={index}>
+          {/* Wenn die Checkbox angeklickt wird, ändert sich der "erledigt"-Status der Aufgabe */}
+          <input
+            type="checkbox"
+            checked={aufgabe.done}
+            onChange={() => toggleDone(index)}
+          />
+          {/* Der Aufgabentext wird angezeigt */}
+          <span>{aufgabe.text}</span>
+        </div>
+      ))}
+      {/* Eingabefeld für eine neue Aufgabe */}
+      <input
+        value={neueAufgabe}
+        onChange={(e) => setNeueAufgabe(e.target.value)}
+        placeholder="Neue Aufgabe hinzufügen..."
+      />
+      {/* Button zum Hinzufügen der neuen Aufgabe */}
+      <button onClick={addAufgabe}>Hinzufügen</button>
     </div>
   );
 }
 
-export default App;
+// Exportiere die Aufgaben-Komponente, damit sie in anderen Teilen der Anwendung verwendet werden kann.
+export default Aufgaben;
